cb0e3f6d176f34a23b649ae9aab1ac75da0d58ce
VectorWolf <mr.joe63@googlemail.com>
2019-06-24 11:08:34 +0200

    Stitch and embed new grass tileset into map

--- data/environment/grassDirt_Set_01/grass_dirt_set_01.tsx
+++ data/environment/grassDirt_Set_01/grass_dirt_set_01.tsx
@@ -1,14 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="grass_dirt_set_01" tilewidth="128" tileheight="128" tilecount="28" columns="7">
- <image source="grassDirt_Set_01.png" width="896" height="512"/>
- <tile id="0">
-  <objectgroup draworder="index">
-   <object id="1" x="0" y="110" width="128" height="18"/>
-  </objectgroup>
- </tile>
+<tileset name="grass_dirt_set_01" tilewidth="128" tileheight="128" tilecount="40" columns="8">
+ <image source="grass_dirt_set_01.png" width="1024" height="640"/>
  <tile id="1">
   <objectgroup draworder="index">
-   <object id="1" x="0" y="110" width="125" height="18"/>
+   <object id="1" x="0" y="110" width="128" height="18"/>
   </objectgroup>
  </tile>
  <tile id="2">
@@ -36,7 +31,57 @@
    <object id="1" x="0" y="110" width="128" height="18"/>
   </objectgroup>
  </tile>
- <tile id="21">
+ <tile id="7">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="15">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="16">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="110" width="128" height="18"/>
+  </objectgroup>
+ </tile>
+ <tile id="17">
   <objectgroup draworder="index">
    <object id="1" x="0" y="110" width="128" height="18"/>
   </objectgroup>
